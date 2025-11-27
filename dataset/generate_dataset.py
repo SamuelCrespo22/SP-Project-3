2a5a671da7ce7e7abad148b680c01a764468cc7a
Miguel202517 <mcds@student.dei.uc.pt>
2025-11-27 16:22:19 +0000

    add dataset

---
+++ dataset/generate_dataset.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+
+def gerar_dataset(n):
+    rng = np.random.default_rng()
+
+    ids = np.arange(1, n + 1)
+
+    salary_cents = rng.integers(500_00, 5000_00, size=n)
+    bonus_cents = rng.integers(0, 1000_00, size=n)
+    hours_per_week = rng.integers(20, 60, size=n)
+    age = rng.integers(18, 70, size=n)
+
+    df = pd.DataFrame({
+        "id": ids,
+        "salary_cents": salary_cents,
+        "bonus_cents": bonus_cents,
+        "hours_per_week": hours_per_week,
+        "age": age,
+    })
+
+    return df
+
+df = gerar_dataset(8192)
+df.to_csv("dataset.csv", index=False)
